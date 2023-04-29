da4f17b2f2765a2d488ebcab7ee3d578e25b943c
IIUPAMET <35329349+IIUPAMET@users.noreply.github.com>
2023-04-29 15:13:26 +0000

    init Telegram bot

---
+++ cmd/feellemon.go
@@ -0,0 +1,75 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	//TeleToken bot
+	TeleToken = os.Getenv("TELE_TOKEN")
+)
+
+// feellemonCmd represents the feellemon command
+var feellemonCmd = &cobra.Command{
+	Use:   "feellemon",
+	Aliases: []string{"start"},
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("kbot $s started", appVersion)
+
+		feellemon, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("check Token,  $s", err)
+			return
+		}
+
+		feellemon.Handle(telebot.OnText, func(m telebot.Context) error {
+
+			log.Println(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm Your bot!"))
+			}
+			return err
+
+		})
+
+		feellemon.Start()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(feellemonCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// feellemonCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// feellemonCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "feellemon/cmd"
+
+func main() {
+	cmd.Execute()
+}
